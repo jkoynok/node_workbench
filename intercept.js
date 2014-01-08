fbb48e1b6b5f9d139fc9614fab9c93e491e0c0ee
OpenShift guest <jkoynok@gmail.com>
2014-01-07 21:38:38 -0500

    add intercept

--- intercept.js
+++ intercept.js
@@ -3,9 +3,9 @@ var line2 = { x1: 3, y1: 2, x2:14, y2: 25};
 
 //y = mx + b
 function GetMB(l) {
-    
-    var m = (l.y2 - l.y1) / (l.x2 = l.x1);
-    var b = y1 - m*x1;
+
+    var m = (l.x2-l.x1 === 0) ? 0 : (l.y2 - l.y1) / (l.x2 - l.x1);
+    var b = l.y1 - m * l.x1;
     return { m: m, b: b};
     
 }
