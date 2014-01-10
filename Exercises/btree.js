359b37af6e6e70a2aa36be74da36ccf7ec0da783
OpenShift guest <jkoynok@gmail.com>
2014-01-10 18:46:53 -0500

    add b-tree traversal, find by level

--- Exercises/btree.js
+++ Exercises/btree.js
@@ -1,29 +1,75 @@
+var node = function(d, n) {
+    this.data = d;
+    this.parent = n;
+};
 
-var node =function (d) {
-    var data = d;
-}
+var root = new node(0, null);
+root.left = new node(1, root);
+root.right = new node(2, root);
 
-var btree = [];
-btree.push(new node(0));
-btree[0].left = new node(1);
-btree[0].right = new node(2);
+root.left.right = new node(3, root.left);
+root.left.left = new node(4, root.left);
 
-btree[0].left.right = new node(3);
-btree[0].left.left = new node(4);
+root.right.right = new node(5, root.right);
+root.right.left = new node(6, root.right);
 
-btree[0].right.right = new node(5);
-btree[0].right.left = new node(6);
+root.left.right.right = new node(7, root.left.right);
+root.left.right.left = new node(8, root.left.right);
 
-btree.push(new node())
+root.left.left.right = new node(9, root.left.left);
+root.left.left.left = new node(10, root.left.left);
 
-btree.push({ data: 0});
+var visited = [];
+var level1 = [];
+var level2 = [];
+var level3 = [];
+var level = 0;
 
-var addnodes = function(tree) {
-    
-    if (tree.length >= 10) {
-        
-        
-        
+Get3(root);
+console.log(level2);
+
+function Get3(n) {
+
+    if (visited.indexOf(n.data) >= 0) {
+        console.log('re-checking ' + n.data);
     }
-}
+    else {
+        console.log('checking ' + n.data +' at level ' + level);
 
+        //mark level
+        switch (level) {
+        case 1:
+            level1.push(n.data);
+            break;
+        case 2:
+            level2.push(n.data);
+            break;
+        case 3:
+            level3.push(n.data);
+            break;
+        default:
+        }
+
+        visited.push(n.data);
+    }
+
+    //check left
+    if (n.left && visited.indexOf(n.left.data) < 0) {
+        level += 1;
+        return Get3(n.left);
+    }
+
+    //check right
+    if (n.right && visited.indexOf(n.right.data) < 0) {
+        level += 1;
+        return Get3(n.right);
+    }
+
+    //root
+    if (!n.parent) return;
+
+    //go back to parent
+    level -= 1;
+    return Get3(n.parent);
+
+}
