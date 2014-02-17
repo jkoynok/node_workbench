d2fe47f01a7b6d69586b4810194f216bffb01287
OpenShift guest <jkoynok@gmail.com>
2014-02-17 10:43:41 -0500

    add tag counter

--- Exercises/Regex.js
+++ Exercises/Regex.js
@@ -1,21 +1,36 @@
-var r = /<(\w+)>/;
-var h = '<a><b></b><c><d></d></c></a><b></b>';
-r.matches = [];
+var http = require('http'),
+    request = require("request"),
+    r = /<(\w+)>/;
+
+r.matches = {};
+r.a = [];
 
 r.getMatches = function(s) {
     var m = this.exec(s);
-    console.log(m);
+
     if (m) {
         this.matches[m[0]] = this.matches[m[0]] ? this.matches[m[0]] + 1 : 1
     }
     else {
+        for (var x in this.matches) {
+            this.a.push({
+                tag: x,
+                count: this.matches[x]
+            });
+        }
+        this.a.sort(function(a, b) {
+            a = a.count; b = b.count;
+            return a < b ? -1 : (a > b ? 1 : 0);
+        });
         return;
     }
-    
-    s = s.slice(m.index+1);
+
+    s = s.slice(m.index + 1);
     return this.getMatches(s);
 }
 
-r.getMatches(h);
+request("http://www.nytimes.com", function(error, response, body) {
+    r.getMatches(body);
+    console.log(r.a);
 
-console.log(r.matches);
+});
