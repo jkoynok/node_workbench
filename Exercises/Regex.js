12064cc0933247b1ffc117d0b87b76e6e7f33ade
OpenShift guest <jkoynok@gmail.com>
2014-04-04 09:43:49 -0400

    add tests for tr

--- Exercises/Regex.js
+++ Exercises/Regex.js
@@ -1,36 +1,8 @@
 var http = require('http'),
     request = require("request"),
-    r = /<(\w+)>/;
+    r = /<(\w+)>/g,
+    rr = new RegExp('<(\w+)>','g');
+var s = '<a><b></b></a><m></m>';
 
-r.matches = {};
-r.a = [];
-
-r.getMatches = function(s) {
-    var m = this.exec(s);
-
-    if (m) {
-        this.matches[m[0]] = this.matches[m[0]] ? this.matches[m[0]] + 1 : 1
-    }
-    else {
-        for (var x in this.matches) {
-            this.a.push({
-                tag: x,
-                count: this.matches[x]
-            });
-        }
-        this.a.sort(function(a, b) {
-            a = a.count; b = b.count;
-            return a < b ? -1 : (a > b ? 1 : 0);
-        });
-        return;
-    }
-
-    s = s.slice(m.index + 1);
-    return this.getMatches(s);
-}
-
-request("http://www.nytimes.com", function(error, response, body) {
-    r.getMatches(body);
-    console.log(r.a);
-
-});
+var res = r.exec(s);
+console.log(res);
--- php/arrays.php
+++ php/arrays.php
@@ -9,9 +9,12 @@ $b = array('a', 'b', 'c', 'd', 'e', 'f');
 $c = array(1,2,34,5,6,4,4,4,4,3,2,1,2,2,2,3,3,4,5,5,5,6,7,7,8);
 //print_r(array_count_values($c));
 
-print_r(array_filter($c, function($x) { return ($x % 2 == 0); }));
+//print_r(array_filter($c, function($x) { return ($x % 2 == 0); }));
 
 
+$t = array(1,2,3,4);
+
+var_dump($t);
 
 
 ?>
